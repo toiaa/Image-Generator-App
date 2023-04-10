8fdb035cf2cab43ae2b4f5378bd0fc37559bd728
toiaa <victoria@geddes.com.ar>
2023-04-10 12:09:21 -0300

    changed backend url from demo localhost to render url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,13 +18,16 @@ const CreatePost = () => {
       // si el form tiene un prompt
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://image-generator-app-1xvs.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
         // enviando el prompt al backend, usamos json.stringify para convertir el objeto a un string
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` }); //guardando y renderizando la imagen
@@ -42,13 +45,16 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          "https://image-generator-app-1xvs.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+          }
+        );
 
         await response.json();
         navigate("/"); // nos lleva al home
